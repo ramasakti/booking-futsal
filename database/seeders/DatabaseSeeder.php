fafdc2cde99c3f0b0d3160d67adb7f5c7433dcb1
Rama Sakti <ramasakti1337@gmail.com>
2025-07-14 14:18:53 +0700

    Seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             InstitusiSeed::class,
             RoleSeed::class,
+            MenuSeed::class,
+            UserSeeder::class
         ]);
     }
 }
